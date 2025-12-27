77b7acb0d01913bc48979f5c94799b4bfb7a8b4e
Ángel <angel.sevilla@uve-group.com>
2025-12-27 23:15:45 +0100

    feat(networking): add vpc

---
+++ provider.tf
@@ -0,0 +1,19 @@
+terraform {
+  cloud {
+    organization = "sevigonz"
+    workspaces {
+      name = "terraform-vcs"
+    }
+  }
+
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~>5.0"
+    }
+  }
+}
+
+provider "aws" {
+  region = "eu-west-1"
+}
