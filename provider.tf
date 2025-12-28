e24b69c6cf34f9874d53501046d371305565dc96
Ángel <angel.sevilla@uve-group.com>
2025-12-28 14:01:45 +0100

    refactor(networking): migrate to private networking module

--- provider.tf
+++ provider.tf
@@ -1,15 +1,16 @@
 terraform {
   cloud {
     organization = "sevigonz"
+
     workspaces {
-      name = "terraform-vcs"
+      name = "terraform-vcs-sevigonz"
     }
   }
 
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = "~>5.0"
+      version = "~> 5.0"
     }
   }
 }
